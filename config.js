2bf10ad8816c6b0781aef7e65ed9369327120c84
ankit123lad-bit <ankit123lad@gmail.com>
2026-07-21 10:01:13 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.MINIX_CONFIG = {
-  supabaseUrl: 'https://jvekyykcsdkbfhvdzjjj.supabase.co',
-  supabaseAnonKey: 'sb_publishable_49dlQ8zz90YQ7aX-A5I_YA_0eYpRReH',
-  autoRefreshSeconds: 60
-};
